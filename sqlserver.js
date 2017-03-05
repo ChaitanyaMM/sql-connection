4472145e6ae9694200a4e0568ef83305e6ccac10
ChaitanyaMM <krishnachaitu.17@gmail.com>
2017-03-05 23:00:36 +0530

    new changes commit

--- sqlserver.js
+++ sqlserver.js
@@ -15,13 +15,6 @@ var db;
 	sql.connect(config, function (err) {
 	if(err){
 		console.log("connection error "+err);
-		sql.connect(config,function(err){
-			if(err){
-				console.log("second time conection err "+err);
-			}else{
-				console.log("Connection established 2nd time");
-			}
-		});
 	}else{
 		console.log("connection established");
 		console.log("sql "+sql);
@@ -191,4 +184,161 @@ app.get('/getsub/:id',function(req,res){
     }).catch(function(err) {
 		console.log("fetching err "+err);
     });
+});
+
+app.get('/getsubscribers',function(req,res){
+	var request=new db.Request();
+	//request.input('subr_no_i', sql.Int(20), req.params.id);
+	request.query('select brn_id_vc ,grp_name_vc ,isnull(subr_no_i,0) as subr_no_i ,isnull(subr_name_vc,0) as subr_name_vc,isnull(subr_mobile_vc,subr_phno_vc) as subr_mobile_vc from tblmastsubscriber').then(function(recordset) {
+		//console.log("pass "+recordset[0].mob_password_vc);
+		res.json(recordset);
+		
+		
+    }).catch(function(err) {
+		console.log("fetching err "+err);
+    });
+});
+
+app.post('/getinstallments',function(req,res){
+	var request=new db.Request();
+	request.input('brn_id_vc', sql.VarChar(20), req.body.selectedbranch);
+	request.input('grp_name_vc', sql.VarChar(20), req.body.selectedgroup);
+	request.input('subr_no_i', sql.Int(20), req.body.selectedsub);
+	
+	request.query('select max(adj_end_inst_i) as adj_end_inst_i from tbltrnreceipts where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND subr_no_i =@subr_no_i').then(function(recordset) {
+		console.log("kjn "+recordset[0].adj_end_inst_i);
+		request.input('auc_no_i', sql.Int(20), recordset[0].adj_end_inst_i);
+		request.query('select convert(varchar(10), auc_date_d,103) as auc_date_d,auc_no_i,auc_intamount_n from tbltrnauctions where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND auc_no_i>@auc_no_i').then(function(record){
+				//res.send(record);
+				request.query('select isnull(sum(isnull(auc_intamount_n,0)),0) as auc_intamount_n from tbltrnauctions where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND auc_no_i>@auc_no_i').then(function(count){
+				//res.send(record);
+					res.json({"data":record,"count":count});
+				}).catch(function(err){
+					console.log("er "+err);
+				}); 
+		}).catch(function(err){
+			console.log("er "+err);
+		}); 
+		//res.send(recordset);
+	}).catch(function(err) {
+		console.log("fetching err "+err);
+    });
+});
+
+app.post('/getinstallments1',function(req,res){
+	var request=new db.Request();
+	request.input('brn_id_vc', sql.VarChar(20), req.body.selectedbranch);
+	request.input('grp_name_vc', sql.VarChar(20), req.body.selectedgroup);
+	request.input('subr_no_i', sql.Int(20), req.body.selectedsub);
+	request.query('select max(adj_end_inst_i) as adj_end_inst_i from tbltrnreceipts where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND subr_no_i =@subr_no_i').then(function(recordset) {
+		console.log("auc "+recordset[0].adj_end_inst_i);
+		request.input('auc_no_i', sql.Int(20), recordset[0].adj_end_inst_i);
+	request.query('select count(*) from tbltrnauctions where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND subr_no_i =@subr_no_i').then(function(recordset1) {
+		console.log("kjn "+recordset1[0]);
+		request.input('auc_no_i', sql.Int(20), recordset[0].adj_end_inst_i);
+		if(recordset1==0){
+			request.query('select convert(varchar(10),auc_duedate_d,103) as auc_duedate_d,auc_no_i,convert(int,auc_intamount_n) as auc_intamount_n ,penalty  from npspenalty where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND auc_no_i >@auc_no_i').then(function(record) {
+				//res.json(re);
+				request.query('select isnull(sum(isnull(auc_intamount_n,0)),0) as auc_intamount_n from tbltrnauctions where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND auc_no_i>@auc_no_i').then(function(amount){
+					request.query('select isnull(dbo.subrpaidamount (@brn_id_vc,@grp_name_vc,@subr_no_i),0) as subrcramount').then(function(credit){
+						var penalityamount=0;
+						record.forEach(function(re){
+							penalityamount=penalityamount+re.penalty;
+						});
+						res.json({"data":record,"count":amount,"creditamount":credit,"penalityamount":penalityamount});
+					}).catch(function(err){
+						console.log("err "+err);
+					});
+					
+				}).catch(function(err){
+					console.log("scsk "+err);
+				});
+			}).catch(function(err){
+				console.log("err"+err);
+			});
+		}else{
+			request.query('select convert(varchar(10),auc_duedate_d,103) as auc_duedate_d,auc_no_i,convert(int,auc_intamount_n) as auc_intamount_n ,penalty  from pspenalty where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND auc_no_i >@auc_no_i').then(function(record) {
+				//console.log("jj");
+				//res.json(re);
+				request.query('select isnull(sum(isnull(auc_intamount_n,0)),0) as auc_intamount_n from tbltrnauctions where brn_id_vc =@brn_id_vc AND grp_name_vc =@grp_name_vc AND auc_no_i>@auc_no_i').then(function(amount){
+					//res.json({"data":record,"count":amount});
+					request.query('select isnull(dbo.subrpaidamount (@brn_id_vc,@grp_name_vc,@subr_no_i),0) as subrcramount').then(function(credit){
+						var penalityamount=0;
+						record.forEach(function(re){
+							penalityamount=penalityamount+re.penalty;
+						});
+						res.json({"data":record,"count":amount,"creditamount":credit,"penalityamount":penalityamount});
+					}).catch(function(err){
+						console.log("err "+err);
+					});
+				}).catch(function(err){
+					console.log("scsk "+err);
+				});
+			}).catch(function(err){
+				console.log("err"+err);
+			});
+		}
+		//res.send(recordset);
+	}).catch(function(err) {
+		console.log("fetching err "+err);
+    });
+	}).catch(function(err){
+		console.log("hbj "+err);
+	});
+});
+
+app.get('/search/:text',function(req,res){
+	var request=new db.Request();
+	console.log('dcds '+req.params.text);
+	request.input('text', sql.VarChar(20), req.params.text);
+	var value=req.params.text;
+	var query="select brn_id_vc ,grp_name_vc ,isnull(subr_no_i,0) as subr_no_i ,isnull(subr_name_vc,0) as subr_name_vc,isnull(subr_mobile_vc,subr_phno_vc) as subr_mobile_vc from tblmastsubscriber where (subr_mobile_vc LIKE '%"+req.params.text+"%' OR subr_name_vc LIKE '%"+req.params.text+"%' OR subr_phno_vc LIKE '%"+req.params.text+"%')";
+	request.query(query).then(function(re){
+		res.send(re);
+	}).catch(function(err){
+		console.log("err "+err);
+	});
+});
+
+app.post('/pay',function(req,res){
+	var request=new db.Request();
+	var d = new Date();
+    var n = d.toLocaleString();
+	console.log("err "+req.body.amount);
+	//console.log('dcds '+req.params.text);
+	request.input('brn_id_vc', sql.VarChar(20), req.body.branch);
+	request.input('grp_name_vc', sql.VarChar(20), req.body.group);
+	request.input('subr_no_i', sql.Int(20), req.body.sub);
+	request.input('rec_date_d',sql.Date,n);
+	request.input('ent_date_d',sql.Date,n);
+	request.input('rec_amount_n',sql.Numeric,req.body.amount);
+	//var value=req.params.text;
+	request.query('select (isnull(max(isnull(rec_number_n,0)),0) + 1) as rec_number_n from tblmobiledata').then(function(primary){
+		console.log("sc "+primary[0].rec_number_n)
+		request.input('rec_number_n',sql.Numeric,primary[0].rec_number_n);
+		var query='insert into tblmobiledata (brn_id_vc ,grp_name_vc ,subr_no_i ,subr_part_i ,rec_date_d ,ent_date_d ,rec_amount_n ,rec_otheramount_n,coll_id_n,rec_number_n) values (@brn_id_vc,@grp_name_vc,@subr_no_i,0,@rec_date_d,@ent_date_d,@rec_amount_n,0,1,@rec_number_n)';
+	request.query(query).then(function(re){
+		console.log("statusCode: ", res.statusCode);
+		if(res.statusCode='200'){
+			res.json({"message":"Payment is success","status":200});
+		}else{
+			res.json({"message":"Payment failed","status":202});
+		}
+		res.send(re);
+	}).catch(function(err){
+		console.log("errn "+err);
+	});
+	}).catch(function(err){
+		console.log("errk "+err);
+	});
+	
+});
+
+app.get('/getpayments',function(req,res){
+	var request=new db.Request();
+	request.query('select * from tblmobiledata').then(function(re){
+		res.send(re);
+	}).catch(function(err){
+		console.log("err "+err);
+	});
 });
